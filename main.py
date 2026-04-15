f0634a48a73eb4d160b2fd0bb5124d241a54dc32
zahidhussain <zahidhussain0853@gmail.com>
2026-04-15 14:39:23 +0100

    rede[ploy test

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import psycopg2
 from dotenv import load_dotenv
 from ta.momentum import RSIIndicator
 
+print("🚀 NEW DEPLOY TEST")
+
 # ==============================
 # 🔐 LOAD ENV VARIABLES
 # ==============================
@@ -16,6 +18,8 @@ load_dotenv()
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+print("DB URL:", DATABASE_URL)
 
 # ==============================
 # 🗄️ DATABASE CONNECTION
